ea7161668e91aa5cbc1159f057b4be5479c4b83b
JustALawnGnome7 <miles.ramage@yahoo.com>
2021-11-03 12:52:57 -0500

    Added workspace

--- AssembliesAndNamespaces/Program.cs
+++ AssembliesAndNamespaces/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace AssembliesAndNamespaces
 {
@@ -6,7 +7,7 @@ namespace AssembliesAndNamespaces
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
         }
     }
 }
